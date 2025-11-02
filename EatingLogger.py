94e0f3d5fab1ee6730356ae2cbab205a048ee54f
JBRattus <sendjbatemail@gmail.com>
2025-11-02 23:41:27 +0000

    Beginning of code

---
+++ EatingLogger.py
@@ -0,0 +1,6 @@
+'''
+The purpose of this program is to take the common foods I eat and create daily tallies of caloric intake and protein. 
+It takes input from a basic GUI and converts that into JSON data that is then loaded for display, allowing me to export and import logs.
+It must use GUI elements, who wants to use the command line for input? Tacky!
+'''
+
